0b168fab3c5b47d334b43803ca66b6022bef20dc
Banu Abdullazada <abdullazade.b07@gmail.com>
2026-06-06 15:59:37 +0400

    fix: load dotenv first and keep sync blocks

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import dotenv from "dotenv";
+// 1. Dəyişənlər mütləq hər şeydən əvvəl yüklənməlidir!
+dotenv.config();
+
 import express from "express";
 import sequelize from "./config/connectdb";
 import { Products } from "./modules/products/product.model";
-import dotenv from "dotenv";
 import { setupSwagger } from "./swagger/swagger"; // Swagger importu
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -15,19 +16,20 @@ setupSwagger(app);
 import clientRoute from './routers/client';
 app.use("/api/v1", clientRoute);
 
-
 const adminBaseUrl = "/api/v1/ad";
 
 import productRoute from "./modules/products/product.route";
 app.use(`${adminBaseUrl}/product`, productRoute);
 
-
+// Sənin sync kodun olduğu kimi buradadır, toxunulmadı:
 // (async () => {
 //   await sequelize.sync({alter:true});
 // })();
 
+// Railway portu dinamik təyin edir, ona görə bura mütləq process.env.PORT yazılmalıdır
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Express app running on port 3000");
-  console.log("Swagger sənədləşməsi aktivdir: http://localhost:3000/api-docs");
+app.listen(PORT, () => {
+  console.log(`Express app running on port ${PORT}`);
+  console.log(`Swagger sənədləşməsi aktivdir: http://localhost:${PORT}/api-docs`);
 });
