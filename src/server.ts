0137211f85c9213d09560b1afd2a50d84638a10e
Banu Abdullazada <abdullazade.b07@gmail.com>
2026-06-06 16:30:10 +0400

    feat: add welcome message to root path

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,37 @@
 import dotenv from "dotenv";
+// Dəyişənlər mütləq hər şeydən əvvəl yüklənməlidir!
 dotenv.config();
 
 import express from "express";
-import cors from "cors";
 import sequelize from "./config/connectdb";
 import { Products } from "./modules/products/product.model";
-import { setupSwagger } from "./swagger/swagger"; 
+import { setupSwagger } from "./swagger/swagger"; // Swagger importu
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
-// Ana səhifəyə (/) girəndə bura işə düşəcək
-app.get("/", (req, res) => {
-  res.send("<h1>Server canavar kimi işləyir! 🚀</h1><p>Swagger üçün <a href='/api-docs/'>bura klikləyin</a>.</p>");
-});
-
+// Swagger-i işə salırıq (Bütün YAML-ları oxuyacaq)
 setupSwagger(app);
 
 import clientRoute from './routers/client';
 app.use("/api/v1", clientRoute);
+
 
 const adminBaseUrl = "/api/v1/ad";
 
 import productRoute from "./modules/products/product.route";
 app.use(`${adminBaseUrl}/product`, productRoute);
 
-// Sənin sync kodun olduğu kimi qaldı:
+
 // (async () => {
 //   await sequelize.sync({alter:true});
 // })();
 
+
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`Express app running on port ${PORT}`);
+  console.log(`Swagger sənədləşməsi aktivdir: http://localhost:${PORT}/api-docs`);
 });
